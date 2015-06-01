63d2c36caab730ad27404788ddcc33409a2951e7
Mark Provan <mrp4@st-andrews.ac.uk>
2015-06-01 22:49:27 +0100

    Added function box.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function nmpdBtnOnClick() {
   alert(this.innerText);
 }
 
-var btns = document.getElementsByClassName("numpad-button");
+var btns = document.getElementsByClassName("keypad-button");
 for (i = 0; i < btns.length; i++) {
   var btn = btns[i];
   btn.onclick = nmpdBtnOnClick;
